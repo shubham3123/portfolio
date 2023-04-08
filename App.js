ffc15b1ba435cc89bab8a8a280ab08b168ae169f
Shubham Badekar <64009106+shubham3123@users.noreply.github.com>
2023-04-09 00:31:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <body className='body' >
+        <header className="portfolio"> <h1>PORTFOLIO</h1> </header>
+        
+   <p  className='p'> Hello ! My name is Shubham Badekar.</p>
+   <p className='p'> I am currently studying in Second Year of Bsc Information Technology.</p>
+   <p className='p'> I am an avid learner and a tech enthusiast. </p>
+   <p className='p'> I wish to begin my career as a Front-end Software Developer. </p>
+      <img src='shub4.png' className='img' alt="myimage"></img>
+      
+    <a target="_blank" href="https://www.instagram.com/cyber_parker">  <img src='instalogo.png' className='instalogo' ></img> </a>
+     <a taget="_blank" href="https://www.twitter.com/badekarshubham3" > <img src="twitter.png" className='twitterlogo'></img> </a>
+     <img src="cyber forensics.png"  height="300px" width="300px"></img> 
+     <img src="network security certificate.png" width="300px" height="300px" ></img>
+      <footer>
+        <h3 classname="end">Created using HTML, CSS and React Js. </h3>
+      </footer>
+    </body>
+    </div>
+  );
+}
+
+export default App;
